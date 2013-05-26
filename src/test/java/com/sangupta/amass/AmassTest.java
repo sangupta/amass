e87527946a709ad4cda6a6cbd5fb47f3f9966e51
Sandeep Gupta <sandy.pec@gmail.com>
2013-05-26 17:49:12 +0530

    Updated test case to match current syntax.

--- src/test/java/com/sangupta/amass/AmassTest.java
+++ src/test/java/com/sangupta/amass/AmassTest.java
@@ -1,3 +1,24 @@
+/**
+ *
+ * amass - web crawling made easy
+ * Copyright (c) 2011-2013, Sandeep Gupta
+ * 
+ * http://www.sangupta/projects/amass
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * 		http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+
 package com.sangupta.amass;
 
 import com.sangupta.amass.core.AfterCrawlHandler;
@@ -26,14 +47,11 @@ public class AmassTest implements BeforeCrawlHandler, AfterCrawlHandler {
 		amass.crawl("http://www.adobe.com");
 		amass.crawl("http://www.sangupta.com");
 		
-		// print the debug information
-		amass.debugJobInfo();
-		
 		// resume crawling
 		amass.resume();
 		
 		// wait for amass to complete and close down
-		amass.cancelAndShutdown();
+		amass.waitAndShutdown();
 		
 		System.out.println("Done AMASS testing!");
 	}
@@ -46,7 +64,11 @@ public class AmassTest implements BeforeCrawlHandler, AfterCrawlHandler {
 
 	@Override
 	public void afterCrawl(CrawlableURL crawlableURL, int priority, WebResponse response, long time) {
-		System.out.println("Done crawling: " + crawlableURL.getURL());
+		if(response != null) {
+			System.out.println("Done crawling: " + crawlableURL.getURL() + "with response: " + response.getResponseCode());
+		} else {
+			System.out.println("Done crawling: " + crawlableURL.getURL() + " with null response");
+		}
 	}
 
 	@Override
